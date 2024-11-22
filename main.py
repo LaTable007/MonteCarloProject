4c61393601aa9e008400783fe75dc4ba69c371ea
LaTable007 <noah.dubuc@gmail.com>
2024-11-22 14:02:56 +0100

    Rajout du mur dans le calcul des points

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 
 # Constants
 
-crossSectionAbsorp = 0.1
-crossSectionScatter = 0.9
+crossSectionAbsorp = 0.15
+crossSectionScatter = 0.85
 numberPoints = 10000
-numberPointsAlive = numberPoints
-thicknessWall = 1.5
+thicknessWall = 2.5
 
 # Step 1 - Draw the initial coordinates and speed of the n from the source density
 
@@ -16,24 +15,40 @@ pos = np.zeros((numberPoints, 2))
 direction = np.zeros((numberPoints, 2))
 direction[:, 0] = 1
 
-j = 0
 i = 0
-
 numberAbsorp = 0
+numberPointsOutside = 0
+numberPointsBackScatter = 0
 
-while numberAbsorp < numberPointsAlive:
+while numberAbsorp < numberPoints:
     i = 0
     while i < numberPoints:
+        if np.all(direction[i, :] == 0):
+            i += 1
+            continue
         ethaAbsorp = np.random.uniform(0, 1, 1)
         ethaScatter = np.random.uniform(0, 1, 1)
     
         sampleAbsorp = -np.log(ethaAbsorp) / crossSectionAbsorp
         sampleScatter = -np.log(ethaScatter) / crossSectionScatter
-    
-        if direction[i, 0] == 0 and direction[i, 1] == 0:
+
+
+        if pos[i, 0] > thicknessWall:
+            numberPointsOutside += 1
+            numberAbsorp += 1
+            print(numberAbsorp)
+            direction[i, :] = [0, 0]  # Désactive le point
             i += 1
             continue
-
+        
+        if pos[i, 0] < 0:
+            numberPointsBackScatter += 1
+            numberAbsorp += 1
+            print(numberAbsorp)
+            direction[i, :] = [0, 0]  # Désactive le point
+            i += 1
+            continue
+        
         if sampleAbsorp < sampleScatter:
             pos[i, :] = pos[i, :] + sampleAbsorp * direction[i, :]
             direction[i, :] = [0, 0]
@@ -48,6 +63,10 @@ while numberAbsorp < numberPointsAlive:
         i += 1
 
 
+print('Number of points: ', numberPoints)
+print('Number of points outside: ', numberPointsOutside)
+print('Number of points backscattered: ', numberPointsBackScatter)
+
 # Step 2 - Draw it's free flight
 
 # Step 3 - Draw the type of collision
@@ -56,12 +75,14 @@ while numberAbsorp < numberPointsAlive:
 
 # Step 5 - Go to 1 if there are still runs to play
 
-x_limits = (0, thicknessWall*1.5)
+x_limits = (thicknessWall - thicknessWall*1.5, thicknessWall*1.5)
 y_limits = (-thicknessWall*1.5, thicknessWall*1.5)
 
 plt.hist2d(pos[:, 0], pos[:, 1], bins=100, range=[x_limits, y_limits], cmap='viridis')
 plt.colorbar(label='Densité des points')
 
+
+plt.axvline(x=0, color='red', linestyle='--', linewidth=2, label='Début de la région')
 plt.axvline(x=thicknessWall, color='red', linestyle='--', linewidth=2, label='Fin de la région')
 
 plt.xlabel('Position en X')
