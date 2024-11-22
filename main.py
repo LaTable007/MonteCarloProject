e8f7854dcd9d49f72e9f5a61c07712665d9a7592
LaTable007 <noah.dubuc@gmail.com>
2024-11-22 19:29:01 +0100

    Calcul avec dimension prise en compte

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm 
+from utils import printProgressBar
 
 
 # Constants
 
-crossSectionAbsorp = 0.2 # Définition d'une valeur pour la cross Section d'absorption pas tenir compte des dimensions pour l'instant 
-crossSectionScatter = 0.8 # Même chose pour la cross Section de diffusion
+crossSectionAbsorp = 1 # Définition d'une valeur pour la cross Section d'absorption pas tenir compte des dimensions pour l'instant 
+crossSectionScatter = 67 # Même chose pour la cross Section de diffusion
 numberPoints = 100000 # Nombre de neutrons envoyé
-thicknessWall = 6.0 # Épaisseur de la paroi
+thicknessWall = 0.25 # Épaisseur de la paroi
 
 pos = np.zeros((numberPoints, 2)) # Position initiale des neutrons
 direction = np.zeros((numberPoints, 2)) # Direction initiale des neutrons
@@ -18,6 +19,10 @@ i = 0
 numberAbsorp = 0 # Nombre de neutrons absorbés ou qui sont sortie du mur
 numberPointsOutside = 0 # Nombre de neutrons qui sont derrière le mur
 numberPointsBackScatter = 0 # Nombre de neutrons qui sont devant le mur
+
+
+distances_absorp = []  # Pour stocker les distances avant absorption
+distances_scatter = []  # Pour stocker les distances avant diffusion
 
 while numberAbsorp < numberPoints: # Tant que tous les neutrons n'ont pas été absorbés ou sont sorties du mur
     i = 0
@@ -35,7 +40,7 @@ while numberAbsorp < numberPoints: # Tant que tous les neutrons n'ont pas été 
         if pos[i, 0] > thicknessWall: # Si le neutron est derrière le mur
             numberPointsOutside += 1 # On incrémente le nombre de neutrons derrière le mur
             numberAbsorp += 1 # On incrémente le nombre de neutrons absorbés
-            print(numberAbsorp)
+            printProgressBar(numberAbsorp, numberPoints, prefix = 'Progress:', suffix = 'Complete', decimals = 3, length = 50)
             direction[i, :] = [0, 0]  # Désactive le neutron
             i += 1
             continue
@@ -43,7 +48,7 @@ while numberAbsorp < numberPoints: # Tant que tous les neutrons n'ont pas été 
         if pos[i, 0] < 0: # Si le neutron est devant le mur
             numberPointsBackScatter += 1 # On incrémente le nombre de neutrons devant le mur
             numberAbsorp += 1 # On incrémente le nombre de neutrons absorbés
-            print(numberAbsorp)
+            printProgressBar(numberAbsorp, numberPoints, prefix = 'Progress:', suffix = 'Complete', decimals = 3, length = 50)
             direction[i, :] = [0, 0]  # Désactive le neutron
             i += 1
             continue
@@ -52,12 +57,14 @@ while numberAbsorp < numberPoints: # Tant que tous les neutrons n'ont pas été 
             pos[i, :] = pos[i, :] + sampleAbsorp * direction[i, :] # On met à jour la position du neutron
             direction[i, :] = [0, 0] # On désactive le neutron
             numberAbsorp += 1 # On incrémente le nombre de neutrons absorbés
-            print(numberAbsorp)
+            printProgressBar(numberAbsorp, numberPoints, prefix = 'Progress:', suffix = 'Complete', decimals = 3, length = 50)
+            distances_absorp.append(sampleAbsorp)  # Enregistre la distance avant absorption
     
         else: # Sinon le neutron est diffusé
             theta = np.random.uniform(0, 2 * np.pi, 1)[0] # On génère un angle aléatoire entre 0 et 2pi
             direction[i, :] = [np.cos(theta), np.sin(theta)] # On met à jour la direction du neutron
             pos[i, :] = pos[i, :] + sampleScatter * direction[i, :] # On met à jour la position du neutron
+            distances_scatter.append(sampleScatter)  # Enregistre la distance avant diffusion
 
         i += 1
 
@@ -65,6 +72,8 @@ while numberAbsorp < numberPoints: # Tant que tous les neutrons n'ont pas été 
 print('Number of points: ', numberPoints)
 print('Number of points outside: ', numberPointsOutside)
 print('Number of points backscattered: ', numberPointsBackScatter)
+print('Distance before absorption: ', np.mean(distances_absorp))
+print('Distance before scattering: ', np.mean(distances_scatter))
 
 x_limits = (thicknessWall - thicknessWall*1.5, thicknessWall*1.5)
 y_limits = (-thicknessWall*1.5, thicknessWall*1.5)
