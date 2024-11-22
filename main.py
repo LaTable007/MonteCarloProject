3afe4acdab9968e671850428f3dd3effc71b5f22
LaTable007 <noah.dubuc@gmail.com>
2024-11-22 09:33:13 +0100

    Histogramme 1 particule exponentielle décroissante

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Constants
 
 crossSection = 1
 direction = np.array([1, 0])
+numberPoints = 100000
 
 # Step 1 - Draw the initial coordinates and speed of the n from the source density
 
-pos = np.array([0, 0])
-speed = np.array([1, 0])
+pos = np.zeros((numberPoints, 2))
 
-etha = np.random.uniform(0, 1, 1)
-sample = -np.log(etha) / crossSection
+i = 0
 
-pos = pos + sample * direction
+while i < numberPoints:
+    etha = np.random.uniform(0, 1, 1)
+    sample = -np.log(etha) / crossSection
+    pos[i, :] = pos[i, :] + sample * direction
+    i += 1
 
-print(pos)
 
 # Step 2 - Draw it's free flight
 
@@ -25,3 +28,9 @@ print(pos)
 # Step 4 - Deal with next n in memory (if appropriate) and go to 2
 
 # Step 5 - Go to 1 if there are still runs to play
+
+plt.hist(pos[:, 0], bins=30, color='skyblue', edgecolor='black')
+plt.xlabel('Position en X')
+plt.ylabel('Nombre de points')
+plt.title('Distribution des positions en X')
+plt.show()
