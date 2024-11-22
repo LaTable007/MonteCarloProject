e27a8f94c06ca5a72a10c42b1946699dc9f86b0c
LaTable007 <noah.dubuc@gmail.com>
2024-11-22 08:50:11 +0100

    Definition of the algorythm structur

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+# Step 1 - Draw the initial coordinates and speed of the n from the source density
+
+# Step 2 - Draw it's free flight
+
+# Step 3 - Draw the type of collision
+
+# Step 4 - Deal with next n in memory (if appropriate) and go to 2
+
+# Step 5 - Go to 1 if there are still runs to play
