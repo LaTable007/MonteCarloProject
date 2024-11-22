cdc2373d62a89552c4c208348fd3d0039cb71f7e
LaTable007 <noah.dubuc@gmail.com>
2024-11-22 11:56:26 +0100

    Rajout du mure dans l'histogramme

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import matplotlib.pyplot as plt
 
 # Constants
 
-crossSection = 1
+crossSectionAbsorp = 1
+corssSectionScatter = 1
 numberPoints = 100000
+thicknessWall = 0.2
 
 # Step 1 - Draw the initial coordinates and speed of the n from the source density
 
 pos = np.zeros((numberPoints, 2))
 direction = np.zeros((numberPoints, 2))
 direction[:, 0] = 1
-print(direction)
 i = 0
 
 while i < numberPoints:
     etha = np.random.uniform(0, 1, 1)
     theta = np.random.uniform(0, 2 * np.pi, 1)[0]
     direction[i, :] = [np.cos(theta), np.sin(theta)]
-    sample = -np.log(etha) / crossSection
+    sample = -np.log(etha) / crossSectionAbsorp
     pos[i, :] = pos[i, :] + sample * direction[i, :]
     i += 1
 
@@ -32,12 +33,14 @@ while i < numberPoints:
 
 # Step 5 - Go to 1 if there are still runs to play
 
-x_limits = (0, 0.2)
-y_limits = (-0.2, 0.2)
+x_limits = (0, thicknessWall*1.5)
+y_limits = (-thicknessWall*1.5, thicknessWall*1.5)
 
 plt.hist2d(pos[:, 0], pos[:, 1], bins=100, range=[x_limits, y_limits], cmap='viridis')
-
 plt.colorbar(label='Densité des points')
+
+plt.axvline(x=thicknessWall, color='red', linestyle='--', linewidth=2, label='Fin de la région')
+
 plt.xlabel('Position en X')
 plt.ylabel('Position en Y')
 plt.title('Répartition des points dans le plan')
