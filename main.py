ecb63bda06158ada1054da7ef30989b87409b1d9
LaTable007 <noah.dubuc@gmail.com>
2024-11-22 09:17:58 +0100

    Code de base pour une dimension

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import numpy as np
 
+
+# Constants
+
+crossSection = 1
+direction = np.array([1, 0])
+
 # Step 1 - Draw the initial coordinates and speed of the n from the source density
+
+pos = np.array([0, 0])
+speed = np.array([1, 0])
+
+etha = np.random.uniform(0, 1, 1)
+sample = -np.log(etha) / crossSection
+
+pos = pos + sample * direction
+
+print(pos)
 
 # Step 2 - Draw it's free flight
 
