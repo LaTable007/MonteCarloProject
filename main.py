09595ac6429983bd41464e3ba286d67fccf2edc0
LaTable007 <noah.dubuc@gmail.com>
2024-11-22 12:30:00 +0100

    Rajout du scattering

--- main.py
+++ main.py
@@ -4,25 +4,48 @@ import matplotlib.pyplot as plt
 
 # Constants
 
-crossSectionAbsorp = 1
-corssSectionScatter = 1
-numberPoints = 100000
-thicknessWall = 0.2
+crossSectionAbsorp = 0.1
+crossSectionScatter = 0.9
+numberPoints = 10000
+numberPointsAlive = numberPoints
+thicknessWall = 1.5
 
 # Step 1 - Draw the initial coordinates and speed of the n from the source density
 
 pos = np.zeros((numberPoints, 2))
 direction = np.zeros((numberPoints, 2))
 direction[:, 0] = 1
+
+j = 0
 i = 0
 
-while i < numberPoints:
-    etha = np.random.uniform(0, 1, 1)
-    theta = np.random.uniform(0, 2 * np.pi, 1)[0]
-    direction[i, :] = [np.cos(theta), np.sin(theta)]
-    sample = -np.log(etha) / crossSectionAbsorp
-    pos[i, :] = pos[i, :] + sample * direction[i, :]
-    i += 1
+numberAbsorp = 0
+
+while numberAbsorp < numberPointsAlive:
+    i = 0
+    while i < numberPoints:
+        ethaAbsorp = np.random.uniform(0, 1, 1)
+        ethaScatter = np.random.uniform(0, 1, 1)
+    
+        sampleAbsorp = -np.log(ethaAbsorp) / crossSectionAbsorp
+        sampleScatter = -np.log(ethaScatter) / crossSectionScatter
+    
+        if direction[i, 0] == 0 and direction[i, 1] == 0:
+            i += 1
+            continue
+
+        if sampleAbsorp < sampleScatter:
+            pos[i, :] = pos[i, :] + sampleAbsorp * direction[i, :]
+            direction[i, :] = [0, 0]
+            numberAbsorp += 1
+            print(numberAbsorp)
+    
+        else:    
+            theta = np.random.uniform(0, 2 * np.pi, 1)[0]
+            direction[i, :] = [np.cos(theta), np.sin(theta)]
+            pos[i, :] = pos[i, :] + sampleScatter * direction[i, :]
+
+        i += 1
 
 
 # Step 2 - Draw it's free flight
